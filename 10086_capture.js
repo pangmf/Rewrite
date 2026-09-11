7909f1664139adceb75eb121982443958ccc7b5c
pangmf <41246258+pangmf@users.noreply.github.com>
2026-09-11 22:23:04 +0800

    Update 10086_capture.js

--- 10086_capture.js
+++ 10086_capture.js
@@ -15,7 +15,7 @@
  *   script-request-body   → 捕获加密请求体
  *   script-response-body  → 捕获响应头r-token
  *
-  [rewrite_local] 配置：
+  [rewrite_local]
   ^https:\\/\\/client\\.app\\.coc\\.10086\\.cn\\/biz-orange\\/LN\\/uamonekeylogin\\/autoLogin url script-request-header https://raw.githubusercontent.com/pangmf/Rewrite/refs/heads/master/10086_capture.js
   ^https:\\/\\/clientaccess\\.10086\\.cn\\/biz-orange\\/BN\\/userInformationService\\/getUserInformation url script-request-header https://raw.githubusercontent.com/pangmf/Rewrite/refs/heads/master/10086_capture.js
   ^https:\\/\\/client\\.app\\.coc\\.10086\\.cn\\/leadeon-abilityopen-biz\\/BN\\/obtainToken\\/getBigNetToken url script-request-header https://raw.githubusercontent.com/pangmf/Rewrite/refs/heads/master/10086_capture.js
